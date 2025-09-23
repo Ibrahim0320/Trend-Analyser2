06c9c5a67bddf162bc32736425707895762f1587
Ibrahim0320 <ibrahim.muhammad02@outlook.com>
2025-09-23 19:20:01 +0200

    trying to finalise work

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,17 +1,7 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.jsx';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./styles.css";
 
-// Absolutely require the #root element (see index.html)
-const el = document.getElementById('root');
-if (!el) {
-  // Fail loudly (you'd otherwise see a blank page)
-  throw new Error('Missing <div id="root"></div> in index.html');
-}
-
-const root = createRoot(el);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const rootEl = document.getElementById("root");
+createRoot(rootEl).render(<App />);
